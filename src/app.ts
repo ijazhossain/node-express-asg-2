3fb111d0c64e900c4533b733389920d009bea2a5
ijaz35 <ijaz.hossain1@gmail.com>
2023-11-23 01:00:10 +0600

    project setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('NodeJS Express Crud Server Is Running.');
+});
+
+export default app;
